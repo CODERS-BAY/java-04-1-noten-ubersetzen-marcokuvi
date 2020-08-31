17f66e557afcd3e9fef92c0890c5562ad9318fb7
Marco <kuvimarco@gmail.com>
2020-08-31 10:33:52 +0200

    Add Grades Class

---
+++ src/Grades.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Grades {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your grade?");
+        int grade = scan.nextInt();
+
+        switch(grade){
+            case 1:
+                System.out.println("Very Good");
+                break;
+            case 2:
+                System.out.println("Good");
+                break;
+            case 3:
+                System.out.println("Satisfactory");
+                break;
+            case 4:
+                System.out.println("Sufficient");
+                break;
+            case 5:
+                System.out.println("Not Sufficient");
+                break;
+        }
+    }
+}
