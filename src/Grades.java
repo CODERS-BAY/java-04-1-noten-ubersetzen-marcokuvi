43ecc0364c11514acce9e737548a6fede4a1b405
Marco <kuvimarco@gmail.com>
2020-08-31 11:46:46 +0200

    Add Fix Formatting

--- src/Grades.java
+++ src/Grades.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 public class Grades {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("What is your grade?");
         int grade = scan.nextInt();
 
-        switch(grade){
+        switch (grade) {
             case 1:
                 System.out.println("Very Good");
                 break;
